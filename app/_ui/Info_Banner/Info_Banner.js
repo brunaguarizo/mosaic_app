19b4b34356c98e57463facbb198a0752beafbed2
Cesaria Jose <cesariajose@Cesarias-MacBook-Air.local>
2025-04-27 18:50:10 -0700

    info banner css and js adjustments - component

--- app/_ui/Info_Banner/Info_Banner.js
+++ app/_ui/Info_Banner/Info_Banner.js
@@ -1,19 +1,17 @@
 "use client";
 import styles from "./Info_Banner.module.css";
-import LinkButton from "../Link_Button/Link_Button";
 import { use, useState } from "react";
 
 function InfoBanner({ prefix, linkText, linkDirectory, suffix }) {
     return (
         <div className={styles.info_container}>
             <p className={styles.info}>
-                {prefix}
-                <span className={styles.link}>
-                    <LinkButton
-                        LinkText={linkText}
-                        linkDirectory={linkDirectory}
-                    />
-                </span>
+                {prefix}{" "}
+                <a
+                    className={styles.link}
+                    href={linkDirectory}>
+                    {linkText}
+                </a>{" "}
                 {suffix}
             </p>
         </div>
