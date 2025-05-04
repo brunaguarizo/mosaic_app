e82bd332d591e50543758a65ffc2c79a489e8565
Cesaria Jose <cesariajose@Cesarias-MacBook-Air.local>
2025-05-03 19:23:07 -0700

    Portfolio projects screens

---
+++ app/Portfolio/Projects/CapiCafe/page.js
@@ -0,0 +1,83 @@
+"use client";
+import { useState } from "react";
+
+import styles from "@/app/Portfolio/Projects/PortfolioProject.module.css";
+import Headingbar from "@/app/_ui/Heading_Bar/Heading_Bar";
+import DescriptionBox from "@/app/_ui/Description_Box/Description_Box";
+import TaskDropDown from "@/app/_ui/Task_Dropdown/Task_Dropdown";
+import ProjectTitle from "@/app/_ui/Project_Title/Project_Title";
+import TaskList from "@/app/_ui/Task_List/Task_List";
+
+export default function CapiCafeInfo() {
+    return (
+        <div className={styles.container}>
+            <Headingbar
+                heading='Capi Cafe'
+                type='navigation'
+            />
+            <div>
+                <img
+                    className={styles.cover_image}
+                    src='/portfolio/capicafe.png'
+                    alt='fish friends cover image'></img>
+            </div>
+            <div className={styles.descriptions}>
+                <ProjectTitle
+                    title='Capi Cafe'
+                    description='Language Requirements - Must be able to have options for English, Spanish, French, Italian, German, Portuguese, and Mandarin.
+                    Customer Service - The AI must be able to assist users with whatever they need
+                    Speed - The chatbot should be quick to  respond to user requests'
+                    percentage={100}
+                    variant='secondary'
+                    className={styles.project_title}
+                />
+
+                <DescriptionBox
+                    title='Desciption'
+                    description='Dashboard Overview – Quick access to tank conditions & reminders
+                    Water Quality Tracker – pH, ammonia, temperature insights
+                    Fish Compatibility Guide – AI-powered suggestions for tankmates
+                    Community Hub – Connects fish owners for advice & tips'
+                    useCase='secondary'
+                />
+                <DescriptionBox
+                    title='Roles'
+                    description='Backend Developer'
+                    useCase='secondary'
+                />
+                <DescriptionBox
+                    title='Tools Used'
+                    description='Visual Studio Code, GitHub'
+                    useCase='secondary'
+                />
+            </div>
+
+            <TaskDropDown>
+                <div title='Analyze the Website'>
+                    <TaskList
+                        Tasks={[
+                            "Look at the files containing images of the website [Capi-Café_Images.zip]",
+
+                            "Do research on the user group of this app",
+                        ]}
+                    />
+                </div>
+                <div title='Do Research on Users'>
+                    <TaskList
+                        Tasks={[
+                            "make a set of wireframes for the app",
+                            "can either be made with paper or with software ",
+                        ]}
+                    />
+                </div>
+                <div title='Do More Research'>
+                    <TaskList
+                        Tasks={[
+                            "Do research on how AI chatbots work and  the best practices to code one",
+                        ]}
+                    />
+                </div>
+            </TaskDropDown>
+        </div>
+    );
+}
---
+++ app/Portfolio/Projects/Piannato/page.js
@@ -0,0 +1,82 @@
+"use client";
+import { useState } from "react";
+
+import styles from "@/app/Portfolio/Projects/PortfolioProject.module.css";
+import Headingbar from "@/app/_ui/Heading_Bar/Heading_Bar";
+import DescriptionBox from "@/app/_ui/Description_Box/Description_Box";
+import TaskDropDown from "@/app/_ui/Task_Dropdown/Task_Dropdown";
+import ProjectTitle from "@/app/_ui/Project_Title/Project_Title";
+import TaskList from "@/app/_ui/Task_List/Task_List";
+
+export default function PiannattoInfo() {
+    return (
+        <div className={styles.container}>
+            <Headingbar
+                heading='Piannatto'
+                type='navigation'
+            />
+            <div>
+                <img
+                    className={styles.cover_image}
+                    src='/portfolio/piannato.png'
+                    alt='CapiCafe cover image'></img>
+            </div>
+            <div className={styles.descriptions}>
+                <ProjectTitle
+                    title='Piannatto'
+                    description='Piannatto is a sophisticated, luxurious music academy that specializes in piano, guitar and vocal lessons. They have requested a redesign of their brand identity to help modernize their brand and feel trendy.'
+                    percentage={100}
+                    variant='secondary'
+                    className={styles.project_title}
+                />
+
+                <DescriptionBox
+                    title='Desciption'
+                    description='Logo & Brand Identity – Sophisticated yet trendy
+Color & Typography System – Minimalistic and modern, keeping accessibility in mind
+Website & Social Media UI – Luxurious, making sure that people know they aren’t an academy for everyone but for those who can afford their price tag'
+                    useCase='secondary'
+                />
+                <DescriptionBox
+                    title='Roles'
+                    description='Brand Designer, UI Designer'
+                    useCase='secondary'
+                />
+                <DescriptionBox
+                    title='Tools Used'
+                    description='Figma, Adobe Illustrator, Adobe Photoshop'
+                    useCase='secondary'
+                />
+            </div>
+
+            <TaskDropDown>
+                <div title='Get Creative'>
+                    <TaskList
+                        Tasks={[
+                            "Look online for inspiration on design and brand identity",
+
+                            "Read books in typography and design to gain more knowledge",
+                        ]}
+                    />
+                </div>
+                <div title='Find Assets'>
+                    <TaskList
+                        Tasks={[
+                            "Find fonts that can express both luxury and modern vibes",
+                            "Look for graphics that can help elevate the deliverables",
+                            " Search for images that will express the businesses goals the best",
+                        ]}
+                    />
+                </div>
+                <div title='Create the First Designs'>
+                    <TaskList
+                        Tasks={[
+                            "Make the first versions of the posters,brand identity, logo and website",
+                            "Get feedback on your designs if available",
+                        ]}
+                    />
+                </div>
+            </TaskDropDown>
+        </div>
+    );
+}
