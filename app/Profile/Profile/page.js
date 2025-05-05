e1004779dd0161c1548bfbcba286ccd7a32bc5dc
Cesaria Jose <cesariajose@Cesarias-MacBook-Air.local>
2025-05-05 15:39:18 -0700

    Adding the settings navigation header and linking to settings page

--- app/Profile/Profile/page.js
+++ app/Profile/Profile/page.js
@@ -39,7 +39,7 @@ export default function Profile() {
             <StatusBar />
             <Headingbar
                 heading='Profile'
-                type='heading'
+                type='settings'
             />
             <Profile_Cover_Box
                 type='secondary'
