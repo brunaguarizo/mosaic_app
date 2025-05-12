f2f90376d5c9d54e70e226eb9ba7a468912a7b4b
brunaguarizo <b.guarizo@hotmail.com>
2025-05-12 09:30:43 -0700

    Debugging

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
 
     return (
         <div className={styles.container}>
-            {/* Status Bar */}
+            {/* Sgittatus Bar */}
             <StatusBar />
             <button
                 className={styles.skipButton}
